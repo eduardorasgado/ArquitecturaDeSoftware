bdae2b8c1035fcbd1c89d82a08fd06776a49d641
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-22 12:31:09 -0600

    factory pattern: database example, typing every variable in testing.

--- patrones_de_diseno/typescript-patterns/creational-patterns/factory-pattern/database-example/testing.ts
+++ patrones_de_diseno/typescript-patterns/creational-patterns/factory-pattern/database-example/testing.ts
@@ -1,26 +1,29 @@
 import {ConnectionFactory} from "./ConnectionFactory";
+import {Connection} from "./Connection";
 
 class Testing {
     public static main(): void {
         console.log('===FACTORY PATTERN===');
-        let connectionFactory = new ConnectionFactory();
+        let connectionFactory: ConnectionFactory = new ConnectionFactory();
 
-        let mysqlConnector = connectionFactory.getConnection("MYSQL");
+        let mysqlConnector: Connection =
+            connectionFactory.getConnection("MYSQL");
 
         mysqlConnector.connect();
         mysqlConnector.disconnect();
 
-        let PSQLConnector = connectionFactory
+        let PSQLConnector: Connection = connectionFactory
                             .getConnection("POSTGRESQL");
         PSQLConnector.connect();
         PSQLConnector.disconnect();
 
-        let oracleConnector = connectionFactory.getConnection("ORACLE");
+        let oracleConnector: Connection =
+            connectionFactory.getConnection("ORACLE");
         oracleConnector.connect();
         oracleConnector.disconnect();
 
         let provider: string; // undefined
-        let ep = connectionFactory.getConnection(provider);
+        let ep: Connection = connectionFactory.getConnection(provider);
         ep.connect();
         ep.disconnect();
     }
