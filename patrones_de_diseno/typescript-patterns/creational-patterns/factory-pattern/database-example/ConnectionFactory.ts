2096813b4ac3d46a6474727e6309c74f48619998
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-22 12:17:09 -0600

    factory pattern: database example, connection provider factory

---
+++ patrones_de_diseno/typescript-patterns/creational-patterns/factory-pattern/database-example/ConnectionFactory.ts
@@ -0,0 +1,36 @@
+import {Connection} from "./Connection";
+import {EmptyConnection} from "./EmptyConnection";
+import {MySQLConnection} from "./MySQLConnection";
+import {OracleConnection} from "./OracleConnection";
+import {SQLServerConnection} from "./SQLServerConnection";
+import {PostgreSQLConnection} from "./PostgreSQLConnection";
+
+/**
+ * DB connection and handler provider.
+ * @class
+ */
+class ConnectionFactory {
+    constructor() { }
+
+    /**
+     * @description
+     * Return an specific db connection handler.
+     *
+     * @param dbProvider - a relational dbms name
+     */
+    public getConnection(dbProvider: string): Connection {
+
+        if(dbProvider === null) return new EmptyConnection();
+
+        if(dbProvider.toUpperCase() === "MYSQL")
+            return new MySQLConnection();
+        if(dbProvider.toUpperCase() === "ORACLE")
+            return new OracleConnection();
+        if(dbProvider.toUpperCase() === "SQLSERVER")
+            return new SQLServerConnection();
+        if(dbProvider.toUpperCase() === "POSTGRESQL")
+            return new PostgreSQLConnection();
+
+        return new EmptyConnection();
+    }
+}
